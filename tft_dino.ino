fc92181ecb05c4471c4dd8869a91d083f8ce25a4
whatwareweb <59752545+whatwareweb@users.noreply.github.com>
2024-07-27 17:59:27 -0500

    Add scrolling ground

--- tft_dino.ino
+++ tft_dino.ino
@@ -3,12 +3,27 @@
 
 TFT_eSPI tft = TFT_eSPI();
 
+uint16_t transparent = 0;
+TFT_eSprite ground = TFT_eSprite(&tft);
+
+int gameSpeed;
+
 void setup() {
   tft.init();
-  tft.setRotation(1);
+  tft.setRotation(3);
   tft.fillScreen(TFT_WHITE);
+
+  gameSpeed = 2;
+
+  // create ground sprite
+  ground.setColorDepth(1);
+  ground.createSprite(ground_width, ground_height);
+  ground.fillSprite(TFT_WHITE);
+  ground.drawXBitmap(0, 0, ground_bits, ground_width, ground_height, TFT_DARKGREY, TFT_WHITE);
+  ground.setBitmapColor(TFT_DARKGREY, TFT_WHITE);
 }
 
 void loop() {
-
+  ground.pushSprite(0, 225);
+  ground.scroll(-gameSpeed);
 }
