b68d8d811855bec25e4ff595d86fa2986653cef0
whatwareweb <59752545+whatwareweb@users.noreply.github.com>
2024-07-28 16:31:27 -0500

    update ground asset to use less ram and make ground scroll smoothly

--- tft_dino.ino
+++ tft_dino.ino
@@ -2,26 +2,57 @@
 #include "bitmaps.h"
 
 TFT_eSPI tft = TFT_eSPI();
-TFT_eSprite background= TFT_eSprite(&tft);
 
-TFT_eSprite ground= TFT_eSprite(&tft);
+TFT_eSprite background = TFT_eSprite(&tft);
+TFT_eSprite ground = TFT_eSprite(&tft);
+TFT_eSprite dino = TFT_eSprite(&tft);
 
-int gameSpeed = 3;
+bool gameOver;
+int groundPosition;
+int gameSpeed;
+bool inJump;
+int jumpPosition;
+
 
 void setup() {
- tft.init();
- tft.setRotation(3);
+  groundPosition = 0;
+  gameSpeed = 3;
 
- background.createSprite(320, 240);
- ground.createSprite(ground_width, ground_height);
- ground.drawXBitmap(0, 0, ground_bits, ground_width, ground_height, TFT_DARKGREY, TFT_BLACK);
+  tft.init();
+  tft.setRotation(3);
+
+  background.createSprite(320, 240);
+  ground.createSprite(ground_width, ground_height);
+
+  tft.fillScreen(TFT_WHITE);
 }
 
 void loop() {
- background.fillSprite(TFT_WHITE);
+  while (1) {
+    background.createSprite(320,240);
+    background.fillSprite(TFT_WHITE);
 
- ground.pushToSprite(&background, 0, 222, TFT_BLACK);
- ground.scroll(-gameSpeed);
- 
- background.pushSprite(0, 0);
+    ground.createSprite(ground_width, ground_height);
+    scroll_ground();
+
+    background.pushSprite(0, 0);
+    background.deleteSprite();
+  }
+}
+
+void scroll_ground() {
+  groundPosition -= gameSpeed;
+
+  if (groundPosition <= -ground_width) {
+    groundPosition = 0;
+  }
+
+  ground.drawXBitmap(0, 0, ground_bits, ground_width, ground_height, TFT_DARKGREY, TFT_BLACK);
+
+  if (groundPosition <= -ground_width + 320) {
+    ground.pushToSprite(&background, groundPosition, 222, TFT_BLACK);
+    ground.pushToSprite(&background, groundPosition + ground_width, 222, TFT_BLACK);
+  } else {
+    ground.pushToSprite(&background, groundPosition, 222, TFT_BLACK);
+  }
 }
